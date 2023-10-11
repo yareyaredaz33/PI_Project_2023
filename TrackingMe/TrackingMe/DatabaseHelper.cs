4801017aeebb325cca4cadab3883bd935ff9f9f0
Danylo Beshta <danylobeshta@gmail.com>
2023-10-11 16:50:17 +0300

    Project and test sql scripts
    need data base

---
+++ TrackingMe/TrackingMe/DatabaseHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class DatabaseHelper
+{
+    private string connectionString; // Рядок підключення до бази даних
+
+    public DatabaseHelper(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public void PrintDataFromTable(string tableName)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand($"SELECT * FROM {tableName}", connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["ColumnName1"]} - {reader["ColumnName2"]}"); // Замініть на імена колонок з вашої таблиці
+                    }
+                }
+            }
+        }
+    }
+
+    public void PopulateTestData()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            // Додайте ваші SQL запити для створення та заповнення таблиць тестовими даними
+            string createTableQuery = "CREATE TABLE TableName (ColumnName1 datatype, ColumnName2 datatype)";
+            string insertDataQuery = "INSERT INTO TableName (ColumnName1, ColumnName2) VALUES (@Value1, @Value2)";
+
+            using (SqlCommand command = new SqlCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            // Заповнення даними
+            for (int i = 0; i < 50; i++)
+            {
+                using (SqlCommand command = new SqlCommand(insertDataQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Value1", $"Value{i}");
+                    command.Parameters.AddWithValue("@Value2", i);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ TrackingMe/TrackingMe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Your Connection String"; // Замініть на свій рядок підключення
+            DatabaseHelper dbHelper = new DatabaseHelper(connectionString);
+
+            dbHelper.PopulateTestData(); // Заповнити тестовими даними
+            dbHelper.PrintDataFromTable("TableName"); // Друк даних з таблиці
+        }
+    }
+}
