8e4c037c234a6372c96be85697ece463f117e22b
yareyaredaz33 <lovica15@gmail.com>
2023-10-12 23:50:45 +0300

    fixed db connection

--- TrackingMe/TrackingMe/Program.cs
+++ TrackingMe/TrackingMe/Program.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
-
-namespace TrackingMe
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string connectionString = "Your Connection String"; // Замініть на свій рядок підключення
-            DatabaseHelper dbHelper = new DatabaseHelper(connectionString);
 
-            dbHelper.PopulateTestData(); // Заповнити тестовими даними
-            dbHelper.PrintDataFromTable("TableName"); // Друк даних з таблиці
+            var connString = "Host=localhost;Username=postgres;Password=1111;Database=TrackMe";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+
+                using (var selectCmd = new NpgsqlCommand("SELECT app_id FROM application", conn))
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Read the integer value and convert it to a string
+                        int intValue = reader.GetInt32(0);
+                        string valueAsString = intValue.ToString();
+                        Console.WriteLine(valueAsString);
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
         }
     }
 }
