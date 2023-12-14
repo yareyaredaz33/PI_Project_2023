4c27cd795dc8918b702ab9ef700c209bc981f932
danylo <danylobeshta@gmail.com>
2023-12-14 21:11:30 +0200

    fix2

--- TrackMe/TrackMe/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ TrackMe/TrackMe/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace TrackMe {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TrackMe;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TrackMe;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
